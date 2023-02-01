db6e022263280cd4b7ed07f7c6bcd0810d46331d
SpencerCloughley <spencercloughley@gmail.com>
2023-02-01 09:15:16 -0500

    Question 4 Completed

---
+++ src/main/java/com/example/comp1008st1178551/Main.java
@@ -0,0 +1,10 @@
+package com.example.comp1008st1178551;
+
+public class Main {
+    public static void main(String args[]){
+        Car car1 = new Car("Honda","Elantra",60000,2010);
+        Car car2 = new Car("BMW","Jetta",75000,2014);
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+}
